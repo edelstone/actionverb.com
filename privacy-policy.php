6c2e5f5677f07ffbe63a8ede000f92df3cc359c9
Kevin Bombino <kevin@bombino.org>
2015-10-12 17:00:17 -0700

    Initial Commit

---
+++ privacy-policy.php
@@ -0,0 +1,15 @@
+<h1>Action Verb, LLC Master Privacy Policy</h1>
+<p>We recognize that your data is very personal and sensitive. This Privacy Policy explains our commitment to protecting your privacy, with regard to your use of any Action Verb, LLC Web Site or Service.</p>
+<h4>Incorporation of Product Privacy Policies</h4>
+<p>Each of our web properties maintains its own Privacy Policy, which are incorporated here by reference.</p>
+<p>The Privacy Policy of Vae Platform is available at <a href="http://vaeplatform.com/privacy-policy">http://vaeplatform.com/privacy-policy</a>.</p>
+<p>The Privacy Policy of BrickFTP is available at <a href="http://brickftp.com/ftp-privacy-policy">http://brickftp.com/ftp-privacy-policy</a>.</p>
+<h4>U.S.-EU Safe Harbor Framework</h4>
+<ul>
+ <li>Action Verb, LLC complies with the U.S.-EU Safe Harbor Framework and the U.S.-Swiss Safe Harbor Framework as set forth by the U.S. Department of Commerce regarding the collection, use, and retention of personal information from European Union member countries and Switzerland.  Action Verb, LLC has certified that it adheres to the Safe Harbor Privacy Principles of notice, choice, onward transfer, security, data integrity, access, and enforcement.  To learn more about the Safe Harbor program, and to view our certification page, please visit <a target="_blank" href="http://www.export.gov/safeharbor/">http://www.export.gov/safeharbor/</a><br /></li>
+ <li>In compliance with the Safe Harbor Principles, Action Verb, LLC commits to resolve complaints about your privacy and our collection or use of your personal information.  European Union citizens with inquiries or complaints regarding this privacy policy should first contact Action Verb, LLC at:<br /><br />Action Verb, LLC<br />ATTN:  Safe Harbor<br />PO Box 29502 #20898<br />Las Vegas, NV 89126<br /><br />Action Verb, LLC has further committed to refer unresolved privacy complaints under the US-EU or US-Switzerland Safe Harbor Principles to an independent dispute resolution mechanism, the BBB EU SAFE HARBOR, operated by the Council of Better Business Bureaus. If you do not receive timely acknowledgment of your complaint, or if your complaint is not satisfactorily addressed by Action Verb, LLC, please visit the BBB EU SAFE HARBOR web site at  <a target="_blank" href="http://www.bbb.org/us/safe-harbor-complaints">www.bbb.org/us/safe-harbor-complaints</a> for more information and to file a complaint.</li>
+</ul>
+<h4>Reviews</h4>
+<p>Action Verb, LLC regularly reviews its compliance with this policy.  Questions regarding this privacy policy should be sent to:<br /><br />Action Verb, LLC<br />PO Box 29502 #20898<br />Las Vegas, NV 89126<br />info@actionverb.com</p>
+<h4>Last Updated</h4>
+<p>This Master Privacy Policy was last updated May 29, 2012.</p>
