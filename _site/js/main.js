6ed099a26f627787d50713add8e0f5ae62d2f80c
Michael Edelstone <michael.edelstone@gmail.com>
2017-07-04 07:47:47 -0500

    removing _site

--- _site/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $('.hamburger').on('click', function(e) {
-    e.preventDefault();
-    $('.hamburger').toggleClass('is-active');
-    $('.nav').toggleClass('visible');
-  });
-});
